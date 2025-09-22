341493528a1e2503d189a472f2e740193c686c21
Sweni002 <nisesweni@gmail.com>
2025-09-22 09:49:31 +0300

    Initial commit

--- app/api/login_api.py
+++ app/api/login_api.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from pydantic import BaseModel
-from app.models.admin import Admin
+from app.models import Admin ,Client
 from app.config import database
 from app.utils.auth import verify_password, create_access_token
 from app.utils.response import success_response, error_response
@@ -22,9 +22,27 @@ class LoginRequest(BaseModel):
 
 @router.post("/login")
 def login(request: LoginRequest, db: Session = Depends(get_db)):
-    admin = db.query(Admin).filter(Admin.username == request.username).first()
-    if not admin or not verify_password(request.password, admin.hashed_password):
+    # Chercher dans Admin
+    user = db.query(Admin).filter(Admin.username == request.username).first()
+    role = "admin"
+
+    # Si pas trouvé, chercher dans Client
+    if not user:
+        user = db.query(Client).filter(Client.username == request.username).first()
+        role = "client"
+
+    # Vérification du mot de passe
+    if not user or not user.verify_password(request.password):
         return error_response(message="Nom d'utilisateur ou mot de passe incorrect", status_code=401)
 
-    access_token = create_access_token(data={"sub": admin.username})
-    return success_response(data={"access_token": access_token, "token_type": "bearer"})
+    # Générer le token
+    access_token = create_access_token(data={"sub": user.username, "role": role})
+
+    return success_response(
+        data={
+            "access_token": access_token,
+            "token_type": "bearer",
+            "role": role
+        } ,
+             message="Authentification  avec succès"
+    )
